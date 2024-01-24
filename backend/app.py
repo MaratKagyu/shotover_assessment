ee1f2bb0c0fa83305e2802fd51066640713b66de
MaratMS <maratkagyu@gmail.com>
2024-01-24 20:33:10 +0100

    Handle standard API and Validation errors

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,29 @@
 from flask import Flask
 from flask import request
-from services.dog_api_service import DogApiService
+from services.dog_api_service import DogApiService, DogApiException, DogApiInvalidParamsException
 
 app = Flask(__name__)
+
 
 @app.route("/api/data", methods=["GET"])
 def show_dogs():
     count = request.args.get("count", "5")
     dog_api = DogApiService()
 
-    return {
-        "success": "success",
-        "message": dog_api.load_dog_images(count)
-    }
+    try:
+        return {
+            "message": dog_api.load_dog_images(count=count),
+            "status": "success"
+        }
 
+    except DogApiInvalidParamsException as exception:
+        return {
+            "status": "error",
+            "message": str(exception)
+        }, 400
+
+    except DogApiException as exception:
+        return {
+            "status": "error",
+            "message": str(exception)
+        }, 503
--- backend/services/dog_api_service.py
+++ backend/services/dog_api_service.py
@@ -1,12 +1,51 @@
 import json
+import re
 from urllib.request import urlopen
+
+
+class DogApiException(Exception):
+    pass
+
+
+class DogApiInvalidParamsException(Exception):
+    pass
+
 
 class DogApiService:
     def __get_api_url(self, count):
+        # Let's respect free Dog API service, and limit max amount of dogs to 100
+        if (not count.isnumeric()) or (int(count) < 1) or (int(count) > 100):
+            raise DogApiInvalidParamsException("Count must be an integer greater than 0, but less than 101")
+
         return f"https://dog.ceo/api/breeds/image/random/{count}"
 
-    def load_dog_images(self, count):
-        with urlopen(self.__get_api_url(count)) as api_response:
-            data = json.loads(api_response.read().decode())
+    def __extract_breed(self, image_url):
+        # Example: "https://images.dog.ceo/breeds/sheepdog-english/n02105641_4577.jpg"
+        return re.search(
+            pattern="images.dog.ceo/breeds/([^/]+)",
+            string=image_url
+        ).group(1)
 
-        return data.get('message')
+    def load_dog_images(self, count):
+        # Load data from Dog API
+        api_url = self.__get_api_url(count)
+        try:
+            with urlopen(api_url) as api_response:
+                data = json.loads(api_response.read().decode())
+        except Exception:
+            raise DogApiException("Dog API is temporary unavailable")
+
+        dog_image_list = data.get('message')
+        if not isinstance(dog_image_list, list):
+            raise DogApiException("Unexpected response format from Dog API")
+
+        # Extract breed
+        return list(map(
+            lambda image: {
+                # Even though it was told that JS must extract the breed,
+                # I decided to do it here, just because it makes more sense to leave it to the Backend
+                "breed": self.__extract_breed(image),
+                "image": image,
+            },
+            dog_image_list
+        ))
