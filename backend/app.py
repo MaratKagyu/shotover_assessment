c9b98b1acd70ebdcf020f54aefbc1a889dd3875a
MaratMS <maratkagyu@gmail.com>
2024-01-24 20:40:36 +0100

    Handle CORS and standard HTTP errors
    - Add support of dotenv

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,13 @@
+import os
 from flask import Flask
 from flask import request
+from flask_cors import CORS
+from dotenv import load_dotenv
 from services.dog_api_service import DogApiService, DogApiException, DogApiInvalidParamsException
 
+load_dotenv()
 app = Flask(__name__)
+CORS(app, origins=[os.environ.get('CORS_ALLOW_ORIGINS', '')])
 
 
 @app.route("/api/data", methods=["GET"])
@@ -27,3 +32,19 @@ def show_dogs():
             "status": "error",
             "message": str(exception)
         }, 503
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return {
+        "status": "error",
+        "message": "Not Found"
+    }, 404
+
+
+@app.errorhandler(405)
+def not_allowed(error):
+    return {
+        "status": "error",
+        "message": "Method Not Allowed"
+    }, 405
